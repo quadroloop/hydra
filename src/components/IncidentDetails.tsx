d85819475dc2f8493ffb9414d1c8457549c1937f
Bryce Mercines <nplixel@gmail.com>
2020-12-29 20:53:29 +0800

    Deployment changes for Tue Dec 29 20:53:29 PST 2020 ==> On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   README.md
    	modified:   src/App.tsx
    	modified:   src/components/Analytics.tsx
    	modified:   src/components/EventDetails.tsx
    	new file:   src/components/EventMast.tsx
    	modified:   src/components/IncidentDetails.tsx
    	new file:   src/components/LocationUpdates.tsx
    	modified:   src/components/Sidebar.tsx
    	new file:   src/styles/Cards.scss
    	modified:   src/styles/Main.scss
    	modified:   src/styles/Map.scss

--- src/components/IncidentDetails.tsx
+++ src/components/IncidentDetails.tsx
@@ -4,25 +4,12 @@ import { googleMapsAPIKEY } from "./utils";
 import axios from "axios";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import nprogress from "nprogress";
+import LocationUpdates from "./LocationUpdates";
+import EventMast from "./EventMast";
 
 const IncidentDetails = (props: any) => {
   const event = props.data;
   const [locationData, setlocationData] = useState(null);
-
-  // const fetchLocationDetails = () => {
-  //   nprogress.start();
-  //   axios
-  //     .get(`https://maps.googleapis.com/maps/api/geocode/json?`, {
-  //       params: {
-  //         latlng: `${event.coordinates.lat},${event.coordinates.long}`,
-  //         key: googleMapsAPIKEY,
-  //       },
-  //     })
-  //     .then((res) => {
-  //       setlocationData(res.data);
-  //       nprogress.done();
-  //     });
-  // };
 
   useEffect(() => {
     if (props.data) {
@@ -61,21 +48,7 @@ const IncidentDetails = (props: any) => {
         </div>
 
         <div className="fade-in">
-          {event.type === "illegal_fishing" && (
-            <div className="event-alert">
-              <div className="pulse-alert p-2">
-                <i className="la la-fish la-4x" />
-              </div>
-            </div>
-          )}
-
-          {event.type === "emergency" && (
-            <div className="distress-alert">
-              <div className="pulse-alert p-2">
-                <i className="la la-bolt la-4x" />
-              </div>
-            </div>
-          )}
+          <EventMast eventType={event.type} />
 
           <strong className="text-warning">{event.name}</strong>
           <h2 className="pb-2 text-white">
@@ -95,8 +68,42 @@ const IncidentDetails = (props: any) => {
             )}
           </h2>
 
+          <div className="event-details-card">
+            <strong className="event-card-header">
+              <i
+                className={`la la-bullseye mr-1 ${
+                  event.status === "PENDING" ? "bg-warning" : "bg-success"
+                }`}
+              />{" "}
+              Report Status
+            </strong>
+
+            <div className="d-flex justify-content-center py-3">
+              <div className={`status-banner ${event.status}`}>
+                {event.status === "PENDING" ? (
+                  <i className="la la-exclamation-circle fade-in-bottom dl-2" />
+                ) : (
+                  <i className="la la-check-circle fade-in-bottom dl-2" />
+                )}{" "}
+                <span className="fade-in-bottom dl-4">{event.status}</span>
+              </div>
+            </div>
+
+            {event.status === "PENDING" ? (
+              <span className="text-center mb-2">
+                This is pending for confirmation, and might require further
+                investigation.
+              </span>
+            ) : (
+              <span className="text-center mb-2">
+                This report has been confirmed by:{" "}
+                <b className="text-active">{event.verifier}</b>
+              </span>
+            )}
+          </div>
+
           {event && (
-            <div className="event-card">
+            <div className="event-details-card">
               <strong
                 className={`event-card-header ${
                   event.type === "emergency" ? "distress" : ""
@@ -127,7 +134,7 @@ const IncidentDetails = (props: any) => {
             </div>
           )}
 
-          <div className="event-card">
+          <div className="event-details-card">
             <strong className="event-card-header">
               <i className="la la-map-marker mr-1 bg-primary" /> Geolocation
               Info
@@ -139,41 +146,9 @@ const IncidentDetails = (props: any) => {
               <strong>Lat:</strong> {event.coordinates.lat}
             </span>
           </div>
-
-          <div className="event-card">
-            <strong className="event-card-header">
-              <i
-                className={`la la-bullseye mr-1 ${
-                  event.status === "PENDING" ? "bg-warning" : "bg-success"
-                }`}
-              />{" "}
-              Report Status
-            </strong>
-
-            <div className="d-flex justify-content-center py-3">
-              <div className={`status-banner ${event.status}`}>
-                {event.status === "PENDING" ? (
-                  <i className="la la-exclamation-circle" />
-                ) : (
-                  <i className="la la-check-circle" />
-                )}{" "}
-                {event.status}
-              </div>
-            </div>
-
-            {event.status === "PENDING" ? (
-              <span className="text-center mb-2">
-                This is pending for confirmation, and might require further
-                investigation.
-              </span>
-            ) : (
-              <span className="text-center mb-2">
-                This report has been confirmed by:{" "}
-                <b className="text-active">{event.verifier}</b>
-              </span>
-            )}
-          </div>
         </div>
+
+        {event.updates && <LocationUpdates data={event.updates} />}
       </>
     </div>
   );
---
+++ src/components/LocationUpdates.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import moment from "moment";
+
+const LocationUpdates = (props: any) => {
+  const eventUpdates = props.data;
+
+  return (
+    <>
+      <p className="geolocation-header-text mt-2">
+        <i className="la la-project-diagram mr-1" />{" "}
+        <strong>Report Location Updates</strong>
+      </p>
+      <div className="event-location-updates pl-3">
+        <i className="la la-expand line-end top" />
+
+        {eventUpdates.map((data: any, index: number) => {
+          return (
+            <>
+              <div className="event-location-card">
+                <i className="la la-angle-double-down  delta-circle" />
+                <strong className="event-card-header">
+                  <i className="la la-bullseye mr-1 bg-warning" /> Report
+                  Location Update: #{index + 1}
+                </strong>
+                <span>
+                  <strong>Longitude:</strong> {data.coordinates.long}
+                </span>
+                <span>
+                  <strong>Latitude:</strong> {data.coordinates.lat}
+                </span>
+                <span>
+                  <strong>Date:</strong>{" "}
+                  {moment(data.date).format("MMM. D, YYYY - hh:mm:ss A")}
+                </span>
+              </div>
+
+              <div className="distance-tag">
+                <span>
+                  <i className="la la-chevron-circle-right mr-2" />
+                  <strong>from last point:</strong>{" "}
+                  {`${data.distanceFromOrigin.toFixed(3)} km`}
+                </span>
+                <span>
+                  <i className="la la-chevron-circle-right mr-2" />
+                  <strong>from origin:</strong>{" "}
+                  {`${data.distanceFromLastPoint.toFixed(3)} km`}
+                </span>
+                <span
+                  className={`fma-tag ${data.fma_flagged ? "" : "flagged"}`}
+                >
+                  <i className="la la-stream mr-2" />
+                  <strong>FMA:</strong> <span>{data.fma}</span>
+                </span>
+              </div>
+            </>
+          );
+        })}
+        <i className="la la-expand line-end bottom" />
+      </div>
+    </>
+  );
+};
+
+export default LocationUpdates;
